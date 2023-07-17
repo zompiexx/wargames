f8962cdb78262a208b562ede5f56647ca0a61992
Andy Glenn <73035697+zompiexx@users.noreply.github.com>
2023-07-17 15:41:44 +0100

    Add files via upload

--- C/imsai8080.c
+++ C/imsai8080.c
@@ -2,37 +2,51 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#define clrscr() printf("\e[1;1H\e[2J")
+#include <ctype.h>
+
+void clear_screen() {
+    printf("\033[2J\033[H");
+}
+
+void string_to_lowercase(char str[]) {
+    for(int i = 0; str[i]; i++){
+        str[i] = tolower(str[i]);
+    }
+}
+
 int main(){
-	clrscr();
+	clear_screen();
 	char command[1];
-	char schpass[1];
-	char student[1];
-	char grade[2];
+
 	sleep(1);
 	printf("64K CP/M VERS. 2.2 MCL030210-D-F8\n");
 	printf("\n");
 	imsai8080:
 	printf("A>");
 	scanf("%s", command);
-	if(strcmp(command,"cls")==0)
-	{
-		clrscr();
+	if(strcmp(command,"cls")==0) {
+		clear_screen();
 		goto imsai8080;
 	} 
-	if(strcmp(command,"bye")==0)
-	{
+	if(strcmp(command,"bye")==0) {
 		printf("INT disabled and HALT Op-Code reached at 0101\n");
 		goto bye;
 	}
-	if(strcmp(command,"dir")==0)
-	{
+	if(strcmp(command,"dir")==0) {
 		printf("A: BYE     COM : CLS     COM : DIALER  COM: DIR     COM\n");
 		printf("A: KERMIT  COM\n");
 		goto imsai8080;
-	}   
-	if(strcmp(command,"kermit")==0)
-	{
+	} 
+    
+    if(strcmp(command,"dialer")==0) {
+        clear_screen();
+        printf("Starting Dialer Program\n\n");
+        sleep(5);
+        //start dialer program
+		goto imsai8080;
+	} 
+  
+	if(strcmp(command,"kermit")==0) {
 		printf("Kermit-80 v4.11 configured for Generic CP/M-80 with Generic (Dumb) CRT Terminal\n");
 		printf("type selected\n");
 		printf("\n");
@@ -53,83 +67,17 @@ int main(){
 		{
 			goto imsai8080;
 		}
-		if(strcmp(command,"connect")==0)
-		{
+		if(strcmp(command,"connect")==0) {
 			printf("Connected to remote host.  Type Control-C to return\n");
 			printf("type Control-? for command list\n");
 			sleep(2);
 			printf("ATDT3115554855\n");
 			sleep(5);
-			clrscr();
-			printf("PDP 11/270 PRB TIP #45                                                TTY 34/984\n");
-			printf("WELCOME TO THE SEATTLE PUBLIC SCHOOL DISTRICT DATANET\n");
-			printf("\n");
-			schlogon:
-			printf("PLEASE LOGON WITH USER PASSWORD: ");
-			scanf("%s",schpass);
-			if(strcmp(schpass,"pencil")==0)
-			{
-				student:
-				printf("PLEASE ENTER STUDENT NAME: ");
-				scanf("%s",student);
-			}
-			else {
-				printf("INVALID PASSWORD\n");
-				printf("\n");
-				goto schlogon;
-			}
-            if (strcmp(student,"exit")==0)
-            {
-                printf("\n");
-                printf("--DISCONNECTED--\n");
-                printf("\n");
-                goto kermit;
-            }
-			if(strcmp(student,"lightman")==0)
-			{
-				printf("\n");
-				printf("STUDENT RECORD: Lightman, David L.\n");
-				printf("\n");
-				printf("     CLASS #    COURSE TITLE         GRADE    TEACHER    PERIOD    ROOM\n");
-				printf("   ______________________________________________________________________\n");
-				printf("\n");
-				printf("      S-202     BIOLOGY 2              ");
-				printf("F");
-				printf("      LIGGET       3       214\n");
-				printf("      E-314     ENGLISH 11B            D      TURMAN       5       172\n");
-				printf("      H-221     WORLD HISTORY 11B      C      DWYMER       2       108\n");
-				printf("      M-106     TRIG 2                 B      DICKERSON    4       315\n");
-				printf("      PE-02     PHYSICAL EDUCATION     C      COMSTOCK     1       GYM\n");
-				printf("      M-122     CALCULUS 1             B      LOGAN        6       240\n");
-				printf("\n");
-				goto student;
-			}
-			if(strcmp(student,"mack")==0)
-			{
-				printf("\n");
-				printf("STUDENT RECORD: Mack, Jennifer K.\n");
-				printf("\n");
-				printf("     CLASS #    COURSE TITLE         GRADE    TEACHER    PERIOD    ROOM\n");
-				printf("   ______________________________________________________________________\n");
-				printf("\n");
-				printf("      S-202     BIOLOGY 2              ");
-				printf("F");
-				printf("      LIGGET       3       214\n");
-				printf("      E-325     ENGLISH 11B            A      ROBINSON     1       114\n");
-				printf("      H-221     WORLD HISTORY 11B      B      DWYER        2       108\n");
-				printf("      M-104     GEOMETRY 2             D      HALQUIST     4       307\n");
-				printf("      B-107     ECONOMICS              D      MARKS        5       122\n");
-				printf("      PE-02     PHYSICAL EDUCATION     C      COMSTOCK     6       GYM\n");
-				printf("\n");
-				goto student;
-			}
-            else {
-                printf("\n");
-                printf("NO STUDENT RECORD FOUND\n");
-                printf("\n");
-                goto student;
-            }
-		}
+			clear_screen();
+            printf("Connecting to School Computer\n\n");
+            sleep(5);
+            //connect to school computer
+        }
 	goto kermit;   
 	}
 	printf("%s", command);
@@ -139,3 +87,4 @@ int main(){
 	bye:
 	return 0;
 }
+
