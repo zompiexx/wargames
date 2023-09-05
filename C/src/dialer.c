df8d1c2a4440ec85f1cc37b7e01c657f13fcd545
Andy Glenn <73035697+zompiexx@users.noreply.github.com>
2023-09-05 12:23:14 +0100

    Add files via upload

--- C/src/dialer.c
+++ C/src/dialer.c
@@ -13,6 +13,8 @@
 #define MAX_SYSTEM_NAME_LENGTH 31  // updated to 31, to account for the null-terminating character
 #define MAX_SYSTEM_ACTION_LENGTH 101  // updated to 101, to account for the null-terminating character
 
+static int area_code;
+
 int number_of_systems = 0;  // global variable for the number of systems found in systems_list.txt file
 
 void clear_input_buffer() {
@@ -30,7 +32,37 @@ void clear_screen() {
     printf("\033[2J\033[H");
 }
 
-int read_data_from_file(int system_pfx[], int system_num[], char system_name[][MAX_SYSTEM_NAME_LENGTH], char system_action[][MAX_SYSTEM_ACTION_LENGTH]) {
+void read_or_create_area_code_file() {
+    FILE *file = fopen("default_area_code.txt", "r");
+    
+    if (file != NULL) {
+        // Read the area code from the file if it exists
+        fscanf(file, "%d", &area_code);
+        fclose(file);
+    } else {
+        // If the file doesn't exist, create it and write "311" to it
+        file = fopen("default_area_code.txt", "w");
+        if (file == NULL) {
+            perror("Error creating default_area_code.txt");
+            exit(EXIT_FAILURE);
+        }
+        fprintf(file, "311");
+        area_code = 311; // set the static variable to 311 as default
+        fclose(file);
+    }
+}
+
+void update_area_code() {
+    FILE *file = fopen("default_area_code.txt", "w");
+    if (file == NULL) {
+        perror("Error updating default_area_code.txt");
+        exit(EXIT_FAILURE);
+    }
+    fprintf(file, "%d", area_code); // Use the static variable directly
+    fclose(file);
+}
+
+int read_data_from_file(int system_area_code[], int system_pfx[], int system_num[], char system_name[][MAX_SYSTEM_NAME_LENGTH], char system_action[][MAX_SYSTEM_ACTION_LENGTH]) {
     FILE *file = fopen("systems_list.txt", "r");
     if (file == NULL) {
         printf("Unable to open the file.\n");
@@ -39,7 +71,7 @@ int read_data_from_file(int system_pfx[], int system_num[], char system_name[][M
 
     int valid_records = 0;
     for (int a = 1; a < MAX_RECORDS; a++) {
-        if (fscanf(file, "%d%d%30s%100s", &system_pfx[a], &system_num[a], system_name[a], system_action[a]) == 4) {
+        if (fscanf(file, "%d%d%d%30s%100s", &system_area_code[a], &system_pfx[a], &system_num[a], system_name[a], system_action[a]) == 5) {
             valid_records++;
         } else {
             break;
@@ -53,9 +85,10 @@ int read_data_from_file(int system_pfx[], int system_num[], char system_name[][M
     return valid_records;
 }
 
-void print_arrays(int valid_records, int system_pfx[], int system_num[], char system_name[][MAX_SYSTEM_NAME_LENGTH], char system_action[][MAX_SYSTEM_ACTION_LENGTH]) {
+void print_arrays(int valid_records, int system_area_code[], int system_pfx[], int system_num[], char system_name[][MAX_SYSTEM_NAME_LENGTH], char system_action[][MAX_SYSTEM_ACTION_LENGTH]) {
     for (int a = 1; a <= valid_records; a++) {
         printf("Record %d:\n", a);
+        printf("system_area_code: %d\n", system_area_code[a]);
         printf("system_pfx: %d\n", system_pfx[a]);
         printf("system_num: %d\n", system_num[a]);
         printf("system_name: %s\n", system_name[a]);
@@ -65,7 +98,7 @@ void print_arrays(int valid_records, int system_pfx[], int system_num[], char sy
 }
 
 void read_and_print_systems_found() {
-    int system_pfx[MAX_RECORDS], system_num[MAX_RECORDS];
+    int system_area_code[MAX_RECORDS], system_pfx[MAX_RECORDS], system_num[MAX_RECORDS];
     char system_name[MAX_RECORDS][MAX_SYSTEM_NAME_LENGTH], system_action[MAX_RECORDS][MAX_SYSTEM_ACTION_LENGTH];
     char system_command[200];
 
@@ -78,6 +111,10 @@ void read_and_print_systems_found() {
     int record_count = 0; // Keep track of the number of records read
 
     while(record_count < MAX_RECORDS && !feof(file)) {
+        // Read area_code
+        if (fscanf(file, "%03d", &system_area_code[record_count]) != 1) {
+            break;
+        }
         // Read pfx
         if (fscanf(file, "%03d", &system_pfx[record_count]) != 1) {
             break;
@@ -111,7 +148,7 @@ void read_and_print_systems_found() {
         for(int i=0; i < record_count; i++){
             printf("                           ");
             printf("%c: ", 'A' + i);
-            printf("(311) ");
+            printf("(%03d) ", system_area_code[i]);
             printf("%03d ", system_pfx[i]);
             printf("%04d ", system_num[i]);
             printf("(%s)\n", system_name[i]);
@@ -141,7 +178,7 @@ void read_and_print_systems_found() {
             clear_screen();
             fflush(stdout); // Flush the output buffer to ensure the prompt is displayed
             usleep(1000000);
-            printf("ATDT311%03d%04d\n", system_pfx[index], system_num[index]);
+            printf("ATD%03d%03d%04d\n", system_area_code[index], system_pfx[index], system_num[index]);
             snprintf(system_command, sizeof(system_command), "aplay samples/dtmf-wopr.wav -q");
             system(system_command);
             usleep(250000);
@@ -157,12 +194,12 @@ void read_and_print_systems_found() {
     }
 }
 
-void saveNumber(int prefix, int num, char *system_name, char *system_action) {
+void saveNumber(int area_code, int prefix, int num, char *system_name, char *system_action) {
     char buffer[200], line[200];
     char record[800] = {0}; // Array to store a record from file.
     int lineCount = 0;
 
-    sprintf(buffer, "%03d\n%04d\n%s\n%s\n", prefix, num, system_name, system_action); // convert number to string
+    sprintf(buffer, "%03d\n%03d\n%04d\n%s\n%s\n", area_code, prefix, num, system_name, system_action); // convert number to string
 
     // Open the file in read mode to check for duplicate entries, if file exists.
     FILE* file = fopen("systems_found.txt", "r");
@@ -172,7 +209,7 @@ void saveNumber(int prefix, int num, char *system_name, char *system_action) {
             lineCount++;
 
             // After every 4 lines (a record), compare with buffer
-            if(lineCount % 4 == 0) {
+            if(lineCount % 5 == 0) {
                 if(strcmp(record, buffer) == 0) { // If entry already exists
                     //printf("Duplicate entry, not writing to file.\n");
                     fclose(file); // Always remember to close the file.
@@ -198,7 +235,7 @@ int compareStrings(const void* a, const void* b) {
     return strcmp((char*)a, (char*)b);
 }
 
-void dialer(int system_pfx[], int system_num[], char system_name[][MAX_SYSTEM_NAME_LENGTH], char system_action[][MAX_SYSTEM_ACTION_LENGTH]) {
+void dialer(int system_area_code[], int system_pfx[], int system_num[], char system_name[][MAX_SYSTEM_NAME_LENGTH], char system_action[][MAX_SYSTEM_ACTION_LENGTH]) {
     int data_pfx[] = {437, 936, 399, 437, 767, 399, 936, 767, 437};
     int data_num[] = {1083, 1493, 2364, 2977, 3395, 3582, 3923, 7305, 8739};
     int data_index = 0;
@@ -240,12 +277,39 @@ void dialer(int system_pfx[], int system_num[], char system_name[][MAX_SYSTEM_NA
 
     clear_screen();
 
-    printf("START NUMBER (1 - 9999): ");
+    printf("CURRENT AREA CODE       : %03d\n\n",area_code);
+    printf("AREA CODE    (0 -  999) : ");
+    fgets(input, sizeof(input), stdin);
+
+    // Check if input is just a carriage return
+    if (strlen(input) == 1 && input[0] == '\n') {
+        area_code = area_code; //do nothing, leave area code as is
+    } else {
+        input[strcspn(input, "\n")] = 0; // Remove the newline character from input
+        area_code = atoi(input);  // Converts the string to an integer
+    }
+
+    if (area_code < 1 || area_code > 999) {
+        printf("VALID RANGE (0 - 999)\n");
+        usleep(2000000);
+        goto numcheck;
+    } else if (strlen(input) == 1 && input[0] == '\n') {
+        //do nothing
+    } else {    
+        printf("SAVE AS DEFAULT (Y/N)   : ");
+        fgets(input, sizeof(input), stdin);
+        // Compare the content of the strings using strcmp
+        if (strcmp(input, "Y\n") == 0 || strcmp(input, "y\n") == 0) {
+            update_area_code(); // Update the file with the new value of area_code
+        }
+    }
+
+    printf("\nSTART NUMBER (1 - 9999) : ");
     fgets(input, sizeof(input), stdin);
     input[strcspn(input, "\n")] = 0;
     nd_start = atoi(input);  // Converts the string to an integer
 
-    printf("END NUMBER   (1 - 9999): ");
+    printf("END NUMBER   (1 - 9999) : ");
     fgets(input, sizeof(input), stdin);
     input[strcspn(input, "\n")] = 0;
     nd_end = atoi(input);  // Converts the string to an integer
@@ -293,32 +357,60 @@ void dialer(int system_pfx[], int system_num[], char system_name[][MAX_SYSTEM_NA
 
             if (ln == 0 && i == nd_start) {
                     printf("\033[7m");
-                    printf("(311) %d          (311) %d          (311) %d          (311) %d       ", pf1, pf2, pf3, pf4);
+                    printf("(%03d) %d          (%03d) %d          (%03d) %d          (%03d) %d       ", area_code, pf1, area_code, pf2, area_code, pf3, area_code, pf4);
                     printf("\033[0m\n");
 
-                    printf("\033[%d;%dH%s", 9, 1, "(311) ");
+                    //printf("\033[%d;%dH%s", 9, 1, "(311) ");
+                    printf("\033[%d;%dH(%d) ", 9, 1, area_code);
 
                     fgets(input, sizeof(input), stdin);
-                    input[strcspn(input, "\n")] = 0;
-                    pf1t = atoi(input);
+                    //input[strcspn(input, "\n")] = 0;
+                    // Check if input is just a carriage return
+                    if (strlen(input) == 1 && input[0] == '\n') {
+                        pf1t = pf1; //do nothing, leave area code as is
+                    } else {
+                        input[strcspn(input, "\n")] = 0; // Remove the newline character from input
+                        pf1t = atoi(input);  // Converts the string to an integer
+                    }
 
-                    printf("\033[%d;%dH%s", 9, 20, "(311) ");
-
-                    fgets(input, sizeof(input), stdin);
-                    input[strcspn(input, "\n")] = 0;
-                    pf2t = atoi(input);
-
-                    printf("\033[%d;%dH%s", 9, 39, "(311) ");
-
-                    fgets(input, sizeof(input), stdin);
-                    input[strcspn(input, "\n")] = 0;
-                    pf3t = atoi(input);
-
-                    printf("\033[%d;%dH%s", 9, 58, "(311) ");
+                    //printf("\033[%d;%dH%s", 9, 20, "(311) ");
+                    printf("\033[%d;%dH(%d) ", 9, 20, area_code);
 
                     fgets(input, sizeof(input), stdin);
-                    input[strcspn(input, "\n")] = 0;
-                    pf4t = atoi(input);
+                    //input[strcspn(input, "\n")] = 0;
+                    // Check if input is just a carriage return
+                    if (strlen(input) == 1 && input[0] == '\n') {
+                        pf2t = pf2; //do nothing, leave area code as is
+                    } else {
+                        input[strcspn(input, "\n")] = 0; // Remove the newline character from input
+                        pf2t = atoi(input);  // Converts the string to an integer
+                    }
+
+                    //printf("\033[%d;%dH%s", 9, 39, "(311) ");
+                    printf("\033[%d;%dH(%d) ", 9, 39, area_code);
+
+                    fgets(input, sizeof(input), stdin);
+                    //input[strcspn(input, "\n")] = 0;
+                    // Check if input is just a carriage return
+                    if (strlen(input) == 1 && input[0] == '\n') {
+                        pf3t = pf3; //do nothing, leave area code as is
+                    } else {
+                        input[strcspn(input, "\n")] = 0; // Remove the newline character from input
+                        pf3t = atoi(input);  // Converts the string to an integer
+                    }
+
+                    //printf("\033[%d;%dH%s", 9, 58, "(311) ");
+                    printf("\033[%d;%dH(%d) ", 9, 58, area_code);
+
+                    fgets(input, sizeof(input), stdin);
+                    //input[strcspn(input, "\n")] = 0;
+                    // Check if input is just a carriage return
+                    if (strlen(input) == 1 && input[0] == '\n') {
+                        pf4t = pf4; //do nothing, leave area code as is
+                    } else {
+                        input[strcspn(input, "\n")] = 0; // Remove the newline character from input
+                        pf4t = atoi(input);  // Converts the string to an integer
+                    }
 
                 if (pf1t > 999 || pf2t > 999 || pf3t > 999 || pf4t > 999 || pf1t < 0 || pf2t < 0 || pf3t < 0 || pf4t < 0) {
                     printf("\nINVALID PREFIXES ENTERED - PRESS ENTER KEY TO CONTINUE\n");
@@ -341,22 +433,22 @@ void dialer(int system_pfx[], int system_num[], char system_name[][MAX_SYSTEM_NA
         // Check prefix 1 for hits
         hit = 'N';
         for (n = 1; n < number_of_systems+1; n++) {
-            if ((system_pfx[n] == pf1) && (i == system_num[n])) {
+            if ((system_area_code[n] == area_code) && (system_pfx[n] == pf1) && (i == system_num[n])) {
                 hit = 'Y';
                 hits = hits + 1;
                 break;
             }
         }
         if (hit == 'Y') {
-            printf("\033[7m(311) ");
+            printf("\033[7m(%03d) ",area_code);
             printf("%03d %04d", pf1, i);
             printf("\033[0m     ");
             snprintf(command, sizeof(command), "aplay samples/computer-beeps-short.wav -q");
             system(command);
             // Save the hit to the file
-            saveNumber(pf1, i, system_name[n], system_action[n]);
+            saveNumber(area_code, pf1, i, system_name[n], system_action[n]);
         } else {
-            printf("(311) ");
+            printf("(%03d) ",area_code);
             printf("%03d %04d", pf1, i);
             printf("     ");
             random_number = rand() % 10;
@@ -393,22 +485,22 @@ void dialer(int system_pfx[], int system_num[], char system_name[][MAX_SYSTEM_NA
         // Check prefix 2 for hits
         hit = 'N';
         for (n = 1; n < number_of_systems+1; n++) {
-            if ((system_pfx[n] == pf2) && (i == system_num[n])) {
+            if ((system_area_code[n] == area_code) && (system_pfx[n] == pf2) && (i == system_num[n])) {
                 hit = 'Y';
                 hits = hits + 1;
                 break;
             }
         }
         if (hit == 'Y') {
-            printf("\033[7m(311) ");
+            printf("\033[7m(%03d) ",area_code);
             printf("%03d %04d", pf2, i);
             printf("\033[0m     ");
             snprintf(command, sizeof(command), "aplay samples/computer-beeps-short.wav -q");
             system(command);
             // Save the hit to the file
-            saveNumber(pf2, i, system_name[n], system_action[n]);
+            saveNumber(area_code, pf2, i, system_name[n], system_action[n]);
         } else {
-            printf("(311) ");
+            printf("(%03d) ",area_code);
             printf("%03d %04d", pf2, i);
             printf("     ");
             random_number = rand() % 10;
@@ -445,22 +537,22 @@ void dialer(int system_pfx[], int system_num[], char system_name[][MAX_SYSTEM_NA
         // Check prefix 3 for hits
         hit = 'N';
         for (n = 1; n < number_of_systems+1; n++) {
-            if ((system_pfx[n] == pf3) && (i == system_num[n])) {
+            if ((system_area_code[n] == area_code) && (system_pfx[n] == pf3) && (i == system_num[n])) {
                 hit = 'Y';
                 hits = hits + 1;
                 break;
             }
         }
         if (hit == 'Y') {
-            printf("\033[7m(311) ");
+            printf("\033[7m(%03d) ",area_code);
             printf("%03d %04d", pf3, i);
             printf("\033[0m     ");
             snprintf(command, sizeof(command), "aplay samples/computer-beeps-short.wav -q");
             system(command);
             // Save the hit to the file
-            saveNumber(pf3, i, system_name[n], system_action[n]);
+            saveNumber(area_code, pf3, i, system_name[n], system_action[n]);
         } else {
-            printf("(311) ");
+            printf("(%03d) ",area_code);
             printf("%03d %04d", pf3, i);
             printf("     ");
             random_number = rand() % 10;
@@ -497,22 +589,22 @@ void dialer(int system_pfx[], int system_num[], char system_name[][MAX_SYSTEM_NA
         // Check prefix 4 for hits
         hit = 'N';
         for (n = 1; n < number_of_systems+1; n++) {
-            if ((system_pfx[n] == pf4) && (i == system_num[n])) {
+            if ((system_area_code[n] == area_code) && (system_pfx[n] == pf4) && (i == system_num[n])) {
                 hit = 'Y';
                 hits = hits + 1;
                 break;
             }
         }
         if (hit == 'Y') {
-            printf("\033[7m(311) ");
+            printf("\033[7m(%03d) ",area_code);
             printf("%03d %04d", pf4, i);
             printf("\033[0m\n");
             snprintf(command, sizeof(command), "aplay samples/computer-beeps-short.wav -q");
             system(command);
             // Save the hit to the file
-            saveNumber(pf4, i, system_name[n], system_action[n]);
+            saveNumber(area_code, pf4, i, system_name[n], system_action[n]);
         } else {
-            printf("(311) ");
+            printf("(%03d) ",area_code);
             printf("%03d %04d", pf4, i);
             printf("\n");
             random_number = rand() % 10;
@@ -559,11 +651,13 @@ void dialer(int system_pfx[], int system_num[], char system_name[][MAX_SYSTEM_NA
 }
 
 int main() {
-    int system_pfx[MAX_RECORDS], system_num[MAX_RECORDS];
+    int system_area_code[MAX_RECORDS],system_pfx[MAX_RECORDS], system_num[MAX_RECORDS];
     char system_name[MAX_RECORDS][MAX_SYSTEM_NAME_LENGTH];
     char system_action[MAX_RECORDS][MAX_SYSTEM_ACTION_LENGTH];
 
-    int valid_records = read_data_from_file(system_pfx, system_num, system_name, system_action);
+    read_or_create_area_code_file();
+
+    int valid_records = read_data_from_file(system_area_code, system_pfx, system_num, system_name, system_action);
     if (valid_records == -1) {
         printf("Error reading the file.\n");
         return -1;
@@ -573,7 +667,7 @@ int main() {
     //print_arrays(valid_records, system_pfx, system_num, system_name, system_action);
     //usleep(5000000);
 
-    dialer(system_pfx, system_num, system_name, system_action);  // call the dialer function with appropriate arguments
+    dialer(system_area_code, system_pfx, system_num, system_name, system_action);  // call the dialer function with appropriate arguments
 
     exit(0);
 }
