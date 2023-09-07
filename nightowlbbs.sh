bbe6c8d0b185a4507cf7418a76bd0b7db0164fc8
Andy Glenn <73035697+zompiexx@users.noreply.github.com>
2023-09-07 12:08:27 +0100

    Add files via upload

--- nightowlbbs.sh
+++ nightowlbbs.sh
@@ -3,6 +3,8 @@
 echo NOTE: CTRL+C WILL EXIT SESSION
 sleep 5
 
+clear
+
 /usr/local/bin/vidtex --host nightowlbbs.ddns.net --port 6400
 echo "--DISCONNECTED--"
 sleep 2
--- telstar.sh
+++ telstar.sh
@@ -3,6 +3,8 @@
 echo NOTE: CTRL+C WILL EXIT SESSION
 sleep 5
 
+clear
+
 /usr/local/bin/vidtex --host glasstty.com --port 6502
 echo "--DISCONNECTED--"
 sleep 2
