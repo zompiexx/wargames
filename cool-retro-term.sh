671f7eb22e552a520e53ed44aaaf060ac8c3a987
Andy Glenn <73035697+zompiexx@users.noreply.github.com>
2023-09-08 07:05:06 +0100

    Add files via upload

--- cool-retro-term.sh
+++ cool-retro-term.sh
@@ -2,7 +2,7 @@
 
 cd $HOME/wargames
 
-cool-retro-term -geometry 1100x665 -p "$HOME/wargames/imsai8080.json" -e $HOME/wargames/imsai8080.sh
+cool-retro-term -geometry 1175x700 -p "$HOME/wargames/imsai8080.json" -e $HOME/wargames/imsai8080.sh
 
 
 
