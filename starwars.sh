c0ac7ccab2d816df0cebc34a95421b716c23a6e4
Andy Glenn <73035697+zompiexx@users.noreply.github.com>
2023-09-21 09:02:49 +0100

    Add files via upload

---
+++ starwars.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+clear
+echo "Thanks to Simon Jansen for giving permission for use of"
+echo "Star Wars ASCIIMATION. For more information, please visit"
+echo "the following website:"
+echo 
+echo "Web: https://www.asciimation.co.nz/"
+sleep 10
+./starwars sw1.txt
+sleep 10
+clear
+
+
